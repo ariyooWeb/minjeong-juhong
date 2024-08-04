2759e018eba4f35f7e35f68bfa8513b754cb3aca
ari <ariyoo.web@gmail.com>
2024-08-04 19:51:07 +0900

    design

--- minjung-wedding/src/App.js
+++ minjung-wedding/src/App.js
@@ -1,6 +1,6 @@
-import './app.scss';
-import './reset.css';
-import './index.css';
+import "./app.scss";
+import "./reset.css";
+import "./index.css";
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import AccountModal from "./accountModal";
@@ -13,12 +13,13 @@ import PhotoPage from "./photo";
 import p1 from "./img/p1.jpg";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import KAKAO_ICON from "./img/kakao_icon.png";
-import {CopyToClipboard} from "react-copy-to-clipboard/src";
+import { CopyToClipboard } from "react-copy-to-clipboard/src";
 import { shareKakao } from "./utils/shareKakao.js";
+import SPACE from "./img/space.jpg";
 
 function App() {
   const [accountIndex, setAccountIndex] = useState(undefined);
- 
+
   const closeAccountModal = () => {
     setAccountIndex(undefined);
   };
@@ -34,137 +35,303 @@ function App() {
   return (
     <div className="App">
       <div className="inner">
-        <div className="main-page" style={{ backgroundImage: "url(" + p1 + ")" }}>
+        <div
+          className="main-page"
+          style={{ backgroundImage: "url(" + p1 + ")" }}
+        >
           <div className="main-page-text">
-            <motion.div className="main-page-text-marriage"  animate={{ x: 0, opacity: 1 }}
-    transition={{ type: "spring", duration: 2 }} initial={{
-      x:500, opacity: 0
-    }} whileInView={{
-     x: 0
-    }}>The marriage of</motion.div>
-            <motion.div className="main-page-text-names" animate={{ x: 0, opacity: 1 }}
-    transition={{ type: "spring", duration: 2 }} initial={{
-      x:0, y:100, opacity: 0
-    }} whileInView={{
-      y: 0
-    }}>
-              <div className="main-page-text-names-name">이&nbsp;주&nbsp;홍</div>
-              <i class="fa-solid fa-heart"></i>
-              <div className="main-page-text-names-name">조&nbsp;민&nbsp;정</div>
+            <motion.div
+              className="main-page-text-marriage"
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", duration: 2 }}
+              initial={{
+                x: 500,
+                opacity: 0,
+              }}
+              whileInView={{
+                x: 0,
+              }}
+            >
+              The marriage of
             </motion.div>
-            <motion.div className="main-page-text-information" animate={{ x: 0, opacity: 1 }}
-    transition={{ type: "spring", duration: 2 }} initial={{
-      x:0, y:100, opacity: 0
-    }} whileInView={{
-      opacity: 1,
-      y: 0
-    }} exit={{ opacity: 0 }}>
-                <div className="main-page-text-information-item">
-                  2024년 11월 24일 일요일 오후 1시 20분
-                </div>
-                <div className="main-page-text-information-item">
-                  삼산컨벤션웨딩
-                </div>
+            <div className="main-page-text-names">
+              <motion.div
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 0 }}
+                initial={{
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                이
+              </motion.div>
+              <motion.div
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 0.3 }}
+                initial={{
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                주
+              </motion.div>
+              <motion.div
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 0.6 }}
+                initial={{
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                홍
+              </motion.div>
+              <motion.div
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 0.9 }}
+                initial={{
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                <i class="fa-solid fa-heart"></i>
+              </motion.div>
+              <motion.div
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 1.2 }}
+                initial={{
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                조
+              </motion.div>
+              <motion.div
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 1.5 }}
+                initial={{
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                민
+              </motion.div>
+              <motion.div
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 1.8 }}
+                initial={{
+                  opacity: 0,
+                }}
+                whileInView={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+              >
+                정
+              </motion.div>
+            </div>
+            <motion.div
+              className="main-page-text-information"
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", duration: 2 }}
+              initial={{
+                x: 0,
+                y: 100,
+                opacity: 0,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              exit={{ opacity: 0 }}
+            >
+              <div className="main-page-text-information-item">
+                2024년 11월 24일 일요일 오후 1시 20분
+              </div>
+              <div className="main-page-text-information-item">
+                삼산컨벤션웨딩
+              </div>
             </motion.div>
           </div>
         </div>
-        <TextPage/>
-        <PhotoPage/>
+        <TextPage />
+        <PhotoPage />
         <div>
           <div className="location-page-main">
             <div className="location-page-main-title">LOCATION</div>
             <div className="location-page-main-bold">삼산컨벤션웨딩</div>
-            <div className="location-page-main-address">인천 부평구 삼산동 458-1</div>
+            <div className="location-page-main-address">
+              인천 부평구 삼산동 458-1
+            </div>
           </div>
           <Map
-        center={{ lat: 37.50748, lng: 126.7381 }}
-        style={{
-          width: '100%',
-          height: '300px'
-        }}
-        level={5}
-      >
-      
-        <MapMarker
-          style={{ border: 'tranparent' }}
-          position={{ lat: 37.50748, lng: 126.7381 }}
-        >
-        
-          <div
+            center={{ lat: 37.50748, lng: 126.7381 }}
             style={{
-              fontSize: '19px',
-              fontWeight: '700',
-              padding: '2px 0 3px',
-              textAlign: 'center',
-              width: '150px'
+              width: "100%",
+              height: "300px",
             }}
+            level={5}
           >
-            삼산컨벤션웨딩
-          </div>
-        </MapMarker>
-      </Map>
+            <MapMarker
+              style={{ border: "tranparent" }}
+              position={{ lat: 37.50748, lng: 126.7381 }}
+            >
+              <div
+                style={{
+                  fontSize: "19px",
+                  fontWeight: "700",
+                  padding: "2px 0 3px",
+                  textAlign: "center",
+                  width: "150px",
+                }}
+              >
+                삼산컨벤션웨딩
+              </div>
+            </MapMarker>
+          </Map>
         </div>
-        <motion.div className="location-page" animate={{ x: 0, opacity: 1 }}
-    transition={{ type: "spring", duration: 2 }} initial={{
-      x:0, y:100, opacity: 0
-    }} whileInView={{
-      opacity: 1,
-      y: 0
-    }} exit={{ opacity: 0 }}>
+        <motion.div
+          className="location-page"
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", duration: 2 }}
+          initial={{
+            x: 0,
+            y: 100,
+            opacity: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          exit={{ opacity: 0 }}
+        >
           <div className="location-page-way">
             <div className="location-page-way-type">
-              <div className="location-page-way-type-title">시내/시외노선버스 안내</div>
+              <div className="location-page-way-type-title">
+                시내/시외노선버스 안내
+              </div>
               <div className="location-page-way-type-flex">
                 <div className="location-page-way-type-box">
                   <div className="location-page-way-type-box-number">
-                    <div className="location-page-way-type-box-number-type"><div className="location-page-way-type-box-number-type-text">&bull;</div>부평역</div>
-                    <div className="location-page-way-type-box-number-bus">558, 565</div>
+                    <div className="location-page-way-type-box-number-type">
+                      <div className="location-page-way-type-box-number-type-text">
+                        &bull;
+                      </div>
+                      부평역
+                    </div>
+                    <div className="location-page-way-type-box-number-bus">
+                      558, 565
+                    </div>
                   </div>
                 </div>
                 <div className="location-page-way-type-box">
                   <div className="location-page-way-type-box-number">
-                    <div className="location-page-way-type-box-number-type"><div className="location-page-way-type-box-number-type-text">&bull;</div>부평구청</div>
-                    <div className="location-page-way-type-box-number-bus">1200(신촌&#183;홍대) 시외버스</div>
+                    <div className="location-page-way-type-box-number-type">
+                      <div className="location-page-way-type-box-number-type-text">
+                        &bull;
+                      </div>
+                      부평구청
+                    </div>
+                    <div className="location-page-way-type-box-number-bus">
+                      1200(신촌&#183;홍대) 시외버스
+                    </div>
                   </div>
                 </div>
                 <div className="location-page-way-type-box">
                   <div className="location-page-way-type-box-number">
-                    <div className="location-page-way-type-box-number-type"><div className="location-page-way-type-box-number-type-text">&bull;</div>부개역</div>
-                    <div className="location-page-way-type-box-number-bus">67-1, 79, 760(동수역) 시내버스</div>
+                    <div className="location-page-way-type-box-number-type">
+                      <div className="location-page-way-type-box-number-type-text">
+                        &bull;
+                      </div>
+                      부개역
+                    </div>
+                    <div className="location-page-way-type-box-number-bus">
+                      67-1, 79, 760(동수역) 시내버스
+                    </div>
                   </div>
                 </div>
                 <div className="location-page-way-type-box">
                   <div className="location-page-way-type-box-number">
-                    <div className="location-page-way-type-box-number-type"><div className="location-page-way-type-box-number-type-text">&bull;</div>송내역</div>
-                    <div className="location-page-way-type-box-number-bus">37, 87</div>
+                    <div className="location-page-way-type-box-number-type">
+                      <div className="location-page-way-type-box-number-type-text">
+                        &bull;
+                      </div>
+                      송내역
+                    </div>
+                    <div className="location-page-way-type-box-number-bus">
+                      37, 87
+                    </div>
                   </div>
                 </div>
                 <div className="location-page-way-type-box">
                   <div className="location-page-way-type-box-number">
-                    <div className="location-page-way-type-box-number-type"><div className="location-page-way-type-box-number-type-text">&bull;</div>동수역</div>
-                    <div className="location-page-way-type-box-number-bus">760(부개역) 시내버스</div>
+                    <div className="location-page-way-type-box-number-type">
+                      <div className="location-page-way-type-box-number-type-text">
+                        &bull;
+                      </div>
+                      동수역
+                    </div>
+                    <div className="location-page-way-type-box-number-bus">
+                      760(부개역) 시내버스
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div className="location-page-divider"></div>
             <div className="location-page-way-type">
-              <div className="location-page-way-type-title">지하철 이용 안내</div>
-              <div className="location-page-way-type-box">지하철 7호선 삼산체육관역 3번 출구</div>
+              <div className="location-page-way-type-title">
+                지하철 이용 안내
+              </div>
+              <div className="location-page-way-type-box">
+                지하철 7호선 삼산체육관역 3번 출구
+              </div>
             </div>
             <div className="location-page-divider"></div>
             <div className="location-page-way-type">
               <div className="location-page-way-type-title">주차장 안내</div>
-              <div className="location-page-way-type-box">건물 뒷편 지하 1,2층 (동시주차 1000대 가능)</div>
+              <div className="location-page-way-type-box">
+                건물 뒷편 지하 1,2층 (동시주차 1000대 가능)
+              </div>
             </div>
           </div>
         </motion.div>
-        <motion.div className="account-page"  animate={{scale: 1, opacity: 1}}
-    transition={{ type: "spring", duration: 2 }} initial={{
-     opacity: 0, scale: 0
-    }} >
-          <div className="account-page-title">신랑 & 신부에게 마음 전하실 곳</div>
-          <div className="account-page-title-hint">축복의 의미로 축의금을 전달해보세요.</div>
+        <div
+          className="space-page"
+          style={{ backgroundImage: "url(" + SPACE + ")" }}
+        ></div>
+        <motion.div
+          className="account-page"
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ type: "spring", duration: 2 }}
+          initial={{
+            opacity: 0,
+            scale: 0,
+          }}
+        >
+          <div className="account-page-title">
+            신랑 & 신부에게 마음 전하실 곳
+          </div>
+          <div className="account-page-title-hint">
+            축복의 의미로 축의금을 전달해보세요.
+          </div>
           <div className="account-page-box">
             <div
               className="account-page-box-one"
@@ -185,14 +352,32 @@ function App() {
             index={accountIndex}
           />
         </motion.div>
-        <div style={{background: "teal"}}>우리라는 이름으로<br/> 시작하는 삶,<br/> 그 설렘의 순간에<br/> 소중한 분을 초대합니다.</div>
+        <div className="space-text-page">
+          우리라는 이름으로 시작하는 삶, <br />그 설렘의 순간에 소중한 분을
+          초대합니다.
+        </div>
         <div className="footer">
           <div className="footer-sns">
-            <div className="footer-sns-kakao" onClick={() => shareKakao("https://ariyooweb.github.io/minjeong-juhong", "민정 &hearts; 주홍")}>카카오톡으로 청첩장 공유하기 <img src={KAKAO_ICON}/></div>
-            <CopyToClipboard className="footer-sns-copy" text="https://ariyooweb.github.io/minjeong-juhong">
-            <div>청첩장 링크 복사하기<i class="fa-regular fa-copy"></i></div></CopyToClipboard>
+            <div
+              className="footer-sns-kakao"
+              onClick={() =>
+                shareKakao(
+                  "https://ariyooweb.github.io/minjeong-juhong",
+                  "민정 &hearts; 주홍"
+                )
+              }
+            >
+              카카오톡으로 청첩장 공유하기 <img src={KAKAO_ICON} />
+            </div>
+            <CopyToClipboard
+              className="footer-sns-copy"
+              text="https://ariyooweb.github.io/minjeong-juhong"
+            >
+              <div>
+                청첩장 링크 복사하기<i class="fa-regular fa-copy"></i>
+              </div>
+            </CopyToClipboard>
           </div>
-          <div>by ariyoo</div>
         </div>
       </div>
     </div>
--- minjung-wedding/src/textPage.jsx
+++ minjung-wedding/src/textPage.jsx
@@ -1,25 +1,52 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
-function TextPage(){
-return (
-    <div className="text-page"  >
-        <motion.div animate={{ x: 0, opacity: 1 }}
-    transition={{ type: "spring", duration: 2 }} initial={{
-      x:0, y:100, opacity: 0
-    }} whileInView={{
-      opacity: 1,
-      y: 0
-    }}>
+function TextPage() {
+  return (
+    <div className="text-page">
+      <div>
         <div className="text-page-divider"></div>
-          <div className="text-page-content">
-            꼭 잡은 두 손, 마주한 모습 속에서<br/> 기쁨과 슬픔, 때로는 아픔을 함께하며<br/> 자그마한 행복의 계단을 쌓아가겠습니다.<br/> 저희의 이 언약을 함께하시어 축복해 주십시오.
-          </div>
-          <div className="text-page-divider"></div>
-        </motion.div>
-          
+        <div className="text-page-content">
+          <motion.div
+            animate={{ x: 0, y: 0, opacity: 1 }}
+            transition={{ delay: 0, type: "spring", duration: 2 }}
+            initial={{ x: 0, y: 100, opacity: 0 }}
+            whileInView={{ x: 0, y: 0, opacity: 1 }}
+            exit={{ x: 0, y: 100, opacity: 0 }}
+          >
+            꼭 잡은 두 손, 마주한 모습 속에서
+          </motion.div>
+          <motion.div
+            animate={{ x: 0, y: 0 }}
+            transition={{ delay: 1, type: "spring", duration: 2 }}
+            initial={{ x: 0, y: 100 }}
+            whileInView={{ x: 0, y: 0 }}
+            exit={{ x: 0, y: 100 }}
+          >
+            기쁨과 슬픔, 때로는 아픔을 함께하며
+          </motion.div>
+          <motion.div
+            animate={{ x: 0, y: 0 }}
+            transition={{ delay: 2, type: "spring", duration: 2 }}
+            initial={{ x: 0, y: 100 }}
+            whileInView={{ x: 0, y: 0 }}
+            exit={{ x: 0, y: 100 }}
+          >
+            자그마한 행복의 계단을 쌓아가겠습니다.
+          </motion.div>
+          <motion.div
+            animate={{ x: 0, y: 0 }}
+            transition={{ delay: 3, type: "spring", duration: 2 }}
+            initial={{ x: 0, y: 100 }}
+            whileInView={{ x: 0, y: 0 }}
+            exit={{ x: 0, y: 100 }}
+          >
+            저희의 이 언약을 함께하시어 축복해 주십시오.
+          </motion.div>
         </div>
-)
-
+        <div className="text-page-divider"></div>
+      </div>
+    </div>
+  );
 }
 
 export default TextPage;
