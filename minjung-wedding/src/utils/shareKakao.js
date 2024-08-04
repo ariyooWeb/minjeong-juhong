215f26bdc85c15c68ec25e19b1231f880230dd82
ari <ariyoo.web@gmail.com>
2024-08-04 14:22:25 +0900

    favicon

--- minjung-wedding/src/utils/shareKakao.js
+++ minjung-wedding/src/utils/shareKakao.js
@@ -1,18 +1,18 @@
-export const shareKakao = (route, title) => { // url이 id값에 따라 변경되기 때문에 route를 인자값으로 받아줌
+export const shareKakao = (route, title) => { 
     if (window.Kakao) {
       const kakao = window.Kakao;
       if (!kakao.isInitialized()) {
-        kakao.init("a75d16e7ea132d7152ce13b003c6cc24"); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
+        kakao.init("a75d16e7ea132d7152ce13b003c6cc24");
       }
   
       kakao.Link.sendDefault({
-        objectType: "feed", // 카카오 링크 공유 여러 type들 중 feed라는 타입 -> 자세한 건 카카오에서 확인
+        objectType: "feed",
         content: {
           title: "민정 ❤ 주홍",
-          description: "2024.11.24 일요일 오후 1시 20분 삼산컨벤션웨딩", // 인자값으로 받은 title
+          description: "2024년 11월 24일 일요일 오후 1시 20분 삼산컨벤션웨딩",
           imageUrl: "https://ifh.cc/g/sAwh6Z.jpg",
           link: {
-            mobileWebUrl: route, // 인자값으로 받은 route(uri 형태)
+            mobileWebUrl: route,
             webUrl: route
           }
         },
